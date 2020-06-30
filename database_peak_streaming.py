ac0f69502454929913097840657cfb017e1348a8
Eric Hedengren <46898294+EricHedengren@users.noreply.github.com>
2020-06-30 12:02:58 -0600

    Implemented averages
    Code cleanup, faster more accurate data

--- database_peak_streaming.py
+++ database_peak_streaming.py
@@ -5,61 +5,56 @@ import time
 import sqlite3
 from sqlite3 import Error
 
-instrument_ip = '10.0.0.55'    # Enter the correct IP address
-num_of_peaks = 8               # Required
-num_of_ports = 8               # Required for different machines
-streaming_time = 1             # Time increments written to the database INCREASE LENGTH
-database = "peak_data.db"      # Name the database
+instrument_ip = '10.0.0.55'
+num_of_peaks = 8
+num_of_ports = 8
+streaming_time = 86400
 
-async def get_data(conn):
+async def get_data(con):
     while True:
         peak_num = []
-        sensors_num = []
-        for i in range(10):
+        for i in range(500):
             peak_data = await queue.get()
             queue.task_done()
             if peak_data['data']:
                 peak_num.append(list(peak_data['data'].data))
-                if i == 0:
-                    ts = time.time()
-                    sensors_num = []
-                    for port_list in peak_data['data'].channel_slices[:num_of_ports]:
-                        sensors_num.append(len(port_list))
-                    sensors_num.insert(0, ts)
             else:
-                break
+                return
+
+        ts = time.time()
+
+        sensors_num = []
+        for port_list in peak_data['data'].channel_slices[:num_of_ports]:
+            sensors_num.append(len(port_list))
+        sensors_num.insert(0, ts)
 
         average_peak_num = []
-
         for peak in range(len(peak_num[0])):
             current_sensor = []
             for data_list in peak_num:
                 current_sensor.append(data_list[peak])
             average_peak_num.append(np.mean(current_sensor))
 
-        with conn:
-            add_data(conn, sensors_num, average_peak_num)
+        with con:
+            add_data(con, sensors_num, average_peak_num)
 
-def add_data(conn, data, peak_data):
-    data_sql = 'INSERT INTO data(peak_data_id,timestamp,{parameters}) VALUES({question})'.format(parameters = data_parameters, question = data_question)
-    peak_sql = 'INSERT INTO peak_data({parameters}) VALUES({question})'.format(parameters = peak_parameters, question = peak_question)
-
-    cur = conn.cursor()
-    cur.execute(peak_sql, peak_data) # Multiple rows at once?
+def add_data(con, data, peak_data):
+    cur = con.cursor()
+    cur.execute(peak_sql, peak_data)
     data.insert(0, cur.lastrowid)
     cur.execute(data_sql, data)
 
 def create_connection(db_file):
-    conn = None
+    con = None
     try:
-        conn = sqlite3.connect(db_file)
+        con = sqlite3.connect(db_file)
     except Error as e:
         print(e)
-    return conn
+    return con
 
-def create_table(conn, create_table_sql):
+def create_table(con, create_table_sql):
     try:
-        c = conn.cursor()
+        c = con.cursor()
         c.execute(create_table_sql)
     except Error as e:
         print(e)
@@ -71,31 +66,27 @@ data_parameters = ','.join('port'+str(i) for i in range(1,num_of_ports+1))
 peak_table_variables = ','.join('peak'+str(i)+' float UNSIGNED' for i in range(1,num_of_peaks+1))
 data_table_variables = ','.join('port'+str(i)+' smallint UNSIGNED' for i in range(1,num_of_ports+1))
 
-create_peak_data_table = "CREATE TABLE IF NOT EXISTS peak_data (id integer PRIMARY KEY,{});".format(peak_table_variables)
-
-create_data_table = "CREATE TABLE IF NOT EXISTS data (id integer PRIMARY KEY,peak_data_id integer NOT NULL,"+\
+create_peak_data_table = "create table if not exists peak_data (id integer PRIMARY KEY,{});".format(peak_table_variables)
+create_data_table = "create table if not exists data (id integer PRIMARY KEY,peak_data_id integer NOT NULL,"+\
 "timestamp double NOT NULL,{},FOREIGN KEY (peak_data_id) REFERENCES peak_data (id));".format(data_table_variables)
 
-conn = create_connection(database)
+peak_sql = 'INSERT INTO peak_data({parameters}) VALUES({question})'.format(parameters = peak_parameters, question = peak_question)
+data_sql = 'INSERT INTO data(peak_data_id,timestamp,{parameters}) VALUES({question})'.format(parameters = data_parameters, question = data_question)
 
-if conn:
-    create_table(conn, create_peak_data_table)
-    create_table(conn, create_data_table)
+con = create_connection("peak_data.db")
+
+if con:
+    create_table(con, create_peak_data_table)
+    create_table(con, create_data_table)
 else:
     raise Exception("Cannot create the database connection.")
 
-for i in range(2):
-    print('start stream')
-    loop = asyncio.get_event_loop() # Loop the loop
-    queue = asyncio.Queue(maxsize=5, loop=loop)
-    stream_active = True
+loop = asyncio.get_event_loop()
+queue = asyncio.Queue(maxsize=5, loop=loop)
+stream_active = True
 
-    peaks_streamer = hyperion.HCommTCPPeaksStreamer(instrument_ip, loop, queue)
+peaks_streamer = hyperion.HCommTCPPeaksStreamer(instrument_ip, loop, queue)
 
-    loop.create_task(get_data(conn))
-
-    loop.call_later(streaming_time, peaks_streamer.stop_streaming)
-
-    loop.run_until_complete(peaks_streamer.stream_data())
-
-    print('repeat stream')
+loop.create_task(get_data(con))
+loop.call_later(streaming_time, peaks_streamer.stop_streaming)
+loop.run_until_complete(peaks_streamer.stream_data())
