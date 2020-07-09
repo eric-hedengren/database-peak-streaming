7020d8e47b7d5656aea449e36a420fe57cc74717
Eric Hedengren <46898294+EricHedengren@users.noreply.github.com>
2020-07-09 13:13:46 -0600

    Long term data

--- database_peak_streaming.py
+++ database_peak_streaming.py
@@ -9,11 +9,13 @@ import os
 instrument_ip = '10.0.0.55'
 num_of_peaks = 8
 num_of_ports = 8
-streaming_time = 100 # Years long
+st_length = 30 # A week
+lt_increment = 100 # A minute
+streaming_time = 201 # Infinite
 
 async def get_data(con):
     repeat = time.time()
-    big_port_numbers = []; big_peak_data = []
+    st_data=[]; st_peak=[]; lt_data=[]; lt_peak=[]
     while True:
         if time.time()-repeat < 10: # Every day/hour
             peak_num = []
@@ -26,14 +28,12 @@ async def get_data(con):
                 else:
                     return
 
-            ts = time.time()
-
             sensors_num = []
             for port_list in peak_data['data'].channel_slices[:num_of_ports]:
                 sensors_num.append(len(port_list))
-            sensors_num.insert(0, ts)
+            sensors_num.insert(0, time.time())
 
-            big_port_numbers.append(sensors_num)
+            st_data.append(sensors_num)
 
             average_peak_num = []
             for peak in range(len(peak_num[0])):
@@ -42,27 +42,34 @@ async def get_data(con):
                     current_sensor.append(data_list[peak])
                 average_peak_num.append(numpy.mean(current_sensor))
 
-            big_peak_data.append(average_peak_num)
+            st_peak.append(average_peak_num)
 
         else:
             repeat = time.time()
-            add_data(con, big_port_numbers, big_peak_data)
-            delete_data(repeat)
-            big_port_numbers = []; big_peak_data = []
-            export_csv(con)
+            add_data(st_data_sql, st_peak_sql, st_data, st_peak)
+            delete_st_data(repeat)
 
-def add_data(con, data, peak_data):
+            for data in st_data[::lt_increment]:    
+                lt_data.append(data)
+            for peak in st_peak[::lt_increment]:
+                lt_peak.append(peak)
+            add_data(lt_data_sql, lt_peak_sql, lt_data, lt_peak)
+            
+            st_data=[]; st_peak=[]; lt_data=[]; lt_peak=[]
+            export_csv()
+
+def add_data(insert_data, insert_peak, data, peak):
     with con:
-        cur.executemany(peak_sql, peak_data)
-        cur.executemany(data_sql, data)
+        cur.executemany(insert_data, data)
+        cur.executemany(insert_peak, peak)
 
-def delete_data(current_time):
+def delete_st_data(current_time):
     with con:
-        cur.execute('delete from data where '+str(current_time)+'-timestamp > 30')
-        data_id = cur.execute('select id from data limit 1').fetchone()
-        cur.execute('delete from peak_data where id < '+str(data_id[0]))
+        cur.execute('delete from st_data where '+str(current_time)+'-timestamp > '+str(st_length))
+        data_id = cur.execute('select id from st_data limit 1').fetchone()
+        cur.execute('delete from st_peak where id < '+str(data_id[0]))
 
-def export_csv(con):
+def export_csv():
     for table in database_tables:
         cur.execute('select * from '+table+';')
         with open('csv/'+table+'.csv', 'w', newline='') as csv_file:
@@ -70,24 +77,26 @@ def export_csv(con):
             csv_writer.writerow([i[0] for i in cur.description])
             csv_writer.writerows(cur)
 
-def create_table(con, create_table_sql):
-    with con:
-        cur.execute(create_table_sql)
-
-peak_question = ','.join('?' * (num_of_peaks))
 data_question = ','.join('?' * (num_of_ports+1))
-peak_parameters = ','.join('peak'+str(i) for i in range(1,num_of_peaks+1))
+peak_question = ','.join('?' * (num_of_peaks))
 data_parameters = ','.join('port'+str(i) for i in range(1,num_of_ports+1))
-peak_table_variables = ','.join('peak'+str(i)+' float UNSIGNED' for i in range(1,num_of_peaks+1))
+peak_parameters = ','.join('peak'+str(i) for i in range(1,num_of_peaks+1))
 data_table_variables = ','.join('port'+str(i)+' smallint UNSIGNED' for i in range(1,num_of_ports+1))
+peak_table_variables = ','.join('peak'+str(i)+' float UNSIGNED' for i in range(1,num_of_peaks+1))
 
-create_peak_data_table = 'create table if not exists peak_data (id integer PRIMARY KEY,{});'.format(peak_table_variables)
-create_data_table = 'create table if not exists data (id integer PRIMARY KEY,timestamp double NOT NULL,{});'.format(data_table_variables)
+st_create_data_table = 'create table if not exists st_data (id integer PRIMARY KEY,timestamp double NOT NULL,'+data_table_variables+');'
+st_create_peak_table = 'create table if not exists st_peak (id integer PRIMARY KEY,'+peak_table_variables+');'
+lt_create_data_table = 'create table if not exists lt_data (id integer PRIMARY KEY,timestamp double NOT NULL,'+data_table_variables+');'
+lt_create_peak_table = 'create table if not exists lt_peak (id integer PRIMARY KEY,'+peak_table_variables+');'
 
-peak_sql = 'insert into peak_data({parameters}) VALUES({question})'.format(parameters = peak_parameters, question = peak_question)
-data_sql = 'insert into data(timestamp,{parameters}) VALUES({question})'.format(parameters = data_parameters, question = data_question)
+create_tables = (st_create_data_table, st_create_peak_table, lt_create_data_table, lt_create_peak_table)
 
-database_tables = ('peak_data','data')
+st_data_sql = 'insert into st_data(timestamp,{parameters}) VALUES({question})'.format(parameters = data_parameters, question = data_question)
+st_peak_sql = 'insert into st_peak({parameters}) VALUES({question})'.format(parameters = peak_parameters, question = peak_question)
+lt_data_sql = 'insert into lt_data(timestamp,{parameters}) VALUES({question})'.format(parameters = data_parameters, question = data_question)
+lt_peak_sql = 'insert into lt_peak({parameters}) VALUES({question})'.format(parameters = peak_parameters, question = peak_question)
+
+database_tables = ('st_data','st_peak','lt_data','lt_peak')
 
 for folder in ('database','csv'):
     os.makedirs('./'+folder, exist_ok = True)
@@ -95,11 +104,9 @@ for folder in ('database','csv'):
 con = sqlite3.connect('database/peak_data.db')
 cur = con.cursor()
 
-if con:
-    create_table(con, create_peak_data_table)
-    create_table(con, create_data_table)
-else:
-    raise Exception('Cannot create database connection.')
+with con:
+    for table in create_tables:
+        cur.execute(table)
 
 loop = asyncio.get_event_loop()
 queue = asyncio.Queue(maxsize=5, loop=loop)
