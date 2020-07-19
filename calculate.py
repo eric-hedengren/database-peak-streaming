8b4d71756026242049009558b4acd3812d64b8eb
Eric Hedengren <46898294+EricHedengren@users.noreply.github.com>
2020-07-18 21:07:26 -0600

    Updated uncertainties

--- calculate.py
+++ calculate.py
@@ -1,10 +1,11 @@
-gf = .807 # gage factor at 22 C 
-wl = None # wavelength
-iwl = None # initial wavelength
-itp = 'measured' # initial temperature
+itp = 'manually measured' # initial temperature | constant after measured | what temperature is measured
+gf = .807 # gage factor at 22 C | constant after calculated from itp | room temperature?
+metal_constant = 'unknown value' # metal constant | constant | specific to the type of metal sensors are measuring
+alpha = 'unknown variable' # unsure what this variable provides
+wl = None # wavelength | current wavelength data
+iwl = None # initial wavelength | get data first row
 
-
-total_strain = 10**6((wl-iwl)/iwl/gf)
+total_strain = (10**6)*((wl-iwl)/iwl/gf)
 
 temperature = (wl-iwl)/(wl*gf*(metal_constant+alpha)) + itp
 
