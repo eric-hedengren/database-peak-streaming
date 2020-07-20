66f6eff8761e8aace5adfcfe1bf22a02fa711020
Eric Hedengren <46898294+EricHedengren@users.noreply.github.com>
2020-07-20 13:55:14 -0600

    Plotting frequencies

--- analyze_data.py
+++ analyze_data.py
@@ -1,8 +1,9 @@
+from matplotlib import pyplot as plt
 import sqlite3
-import matplotlib
 import csv
 import os
 
+'''
 # don't export csv? instead use lists?
 def export_csv(): # export interpreted data instead?
     for table in database_tables:
@@ -24,7 +25,8 @@ for tup in dt:
 
 export_csv()
 
-'''
+
+
 itp = 'manually measured' # initial temperature | constant after measured | what temperature is measured
 gf = .807 # gage factor at 22 C | constant after calculated from itp | room temperature?
 metal_constant = 'unknown value' # metal constant | constant | specific to the type of metal sensors are measuring
@@ -39,5 +41,51 @@ temperature = (wl-iwl)/(wl*gf*(metal_constant+alpha)) + itp
 strain = total_strain-temperature
 '''
 
-# graph some data
-# subplots for each port
+con = sqlite3.connect('database/peak_data.db')
+cur = con.cursor()
+lt_data = cur.execute('select * from lt_data;').fetchall()
+lt_peak = cur.execute('select * from lt_peak;').fetchall()
+
+timestamp = []
+for data in lt_data:
+    timestamp.append(data[1])
+
+plt.figure() #figsize=(20,14)
+
+'''
+for i in range(1,len(lt_peak[0])):
+    frequency = []
+    for data in lt_peak:
+        frequency.append(data[i])
+    plt.plot(timestamp, frequency)
+'''
+
+# plot calculated strain instead of peak information
+# split data by port for location information
+
+ax1 = plt.subplot(2,1,1)
+ports = lt_data[0]
+number = ports[2]+1
+for i in range(1,number):
+    frequency = [] # make function with (i) input
+    for data in lt_peak:
+        frequency.append(data[i])
+    plt.plot(timestamp, frequency)
+
+previous = number
+number = ports[3]
+ax2 = plt.subplot(2,1,2)
+for i in range(previous, number+previous):
+    frequency = [] # make function with (i) input
+    for data in lt_peak:
+        frequency.append(data[i])
+    plt.plot(timestamp, frequency)
+
+'''
+ax3 = plt.subplot(6,1,3)
+ax4 = plt.subplot(6,2,1)
+ax5 = plt.subplot(6,2,2)
+ax6 = plt.subplot(6,2,3)
+'''
+
+plt.show()
